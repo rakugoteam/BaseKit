24f483d72d5ee75ee42802f0b14b4597e0e1a6de
Jeremi Biernacki <jeremi360@gmail.com>
2022-08-09 21:03:11 +0200

    small fix

--- plug.gd
+++ plug.gd
@@ -17,11 +17,10 @@ func _plugging():
 		"exclude": ["addons/kit/plugin.cfg"],
 		"on_update": "post_kit_update"
 	})
-	
-	## comment-out if of you don't want to update kit itself
-	# plug("your/kit-repo", {"include": ["addons", "plug.gd"]})
+
+	# this will update your kit in other projects
+	plug("your/kit-repo", {"include": ["addons", "plug.gd"]})
 
 func post_kit_update(plugin : Dictionary):
 	directory_copy_recursively("addons/kit/", "addons/your-kit")
-    # this will update your kit
     
