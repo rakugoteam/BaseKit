96e617a68fc8a3021844aa9b1f5afed258c2a8f4
Jeremi Biernacki <jeremi360@gmail.com>
2022-08-09 20:51:40 +0200

    fix plug template

--- plug.gd
+++ plug.gd
@@ -3,16 +3,25 @@ extends "res://addons/gd-plug/plug.gd"
 
 
 func _plugging():
-    # Declare your plugins in here with plug(src, args)
-    # By default, only "addons/" directory will be installed
-    plug("rakugoteam/Emojis-For-Godot", {"include": ["addons", ".import/"]})
-    plug("rakugoteam/Godot-Material-Icons", {"include": ["addons", ".import/"]})
-    plug("rakugoteam/AdvancedText")
-    plug("rakugoteam/Rakugo", {"include": ["addons/rakugo", ".import/"]})
+	# Declare your plugins in here with plug(src, args)
+	# By default, only "addons/" directory will be installed
+	plug("rakugoteam/Emojis-For-Godot", {"include": ["addons", ".import/"]})
+	plug("rakugoteam/Godot-Material-Icons", {"include": ["addons", ".import/"]})
+	plug("rakugoteam/AdvancedText")
+	plug("rakugoteam/Rakugo", {"include": ["addons/rakugo", ".import/"]})
 
-    # this must be changed to kit needs
-    plug("rakugoteam/BaseKit", {
-        "include": ["addons/kit", "addons/gd-plug"],
-        "exclude": ["addons/kit/plugin.cfg"],
-        "install_root": "addons/kit"
-    })
+	# this 2 instruction must be changed to kit needs
+	# this will update base-kit it self
+	plug("rakugoteam/BaseKit", {
+		"include": ["addons/kit", "addons/gd-plug"],
+		"exclude": ["addons/kit/plugin.cfg"],
+		"on_update": "post_kit_update"
+	})
+	
+	## comment-out if of you don't want to update kit itself
+	# plug("your/kit-repo", {"include": ["addons", "plug.gd"]})
+
+func post_kit_update(plugin : Dictionary):
+	directory_copy_recursively("addons/kit/", "addons/your-kit")
+    # this will update your kit
+    
