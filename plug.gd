6bfef58175dfb13d48e809fcfb3c64620ab0580f
Jeremi Biernacki <jeremi360@gmail.com>
2022-08-09 20:15:48 +0200

    make kit path always works

--- plug.gd
+++ plug.gd
@@ -1,4 +1,6 @@
+# this file must be manually copied to kit repo
 extends "res://addons/gd-plug/plug.gd"
+
 
 func _plugging():
     # Declare your plugins in here with plug(src, args)
@@ -7,7 +9,10 @@ func _plugging():
     plug("rakugoteam/Godot-Material-Icons", {"include": ["addons", ".import/"]})
     plug("rakugoteam/AdvancedText")
     plug("rakugoteam/Rakugo", {"include": ["addons/rakugo", ".import/"]})
+
+    # this must be changed to kit needs
     plug("rakugoteam/BaseKit", {
         "include": ["addons/kit", "addons/gd-plug"],
-        "exclude": ["addons/kit/plugin.cfg"]
+        "exclude": ["addons/kit/plugin.cfg"],
+        "install_root": "addons/kit"
     })
